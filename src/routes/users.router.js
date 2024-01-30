58bf5096f4043384dde82fba5d81b38409fde87d
TriumphOne <woo001232naver.com>
2024-01-30 16:37:17 +0900

    사용자 정보 변경 테이블 작성

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,6 +3,7 @@ import { prisma } from "../utils/prisma/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import { Prisma } from "@prisma/client";
 
 const router = express.Router();
 
@@ -18,19 +19,30 @@ router.post("/sign-up", async (req, res, next) => {
       return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.users.create({
-      data: { email, password: hashedPassword },
-    });
 
-    const userInfo = await prisma.usersInfos.create({
-      data: {
-        userId: user.userId,
-        name,
-        age,
-        gender,
-        profileImage,
+    const [user, userInfo] = await prisma.$transaction(
+      async (tx) => {
+        const user = await tx.users.create({
+          data: { email, password: hashedPassword },
+        });
+
+        const userInfo = await tx.usersInfos.create({
+          data: {
+            userId: user.userId,
+            name,
+            age,
+            gender,
+            profileImage,
+          },
+        });
+
+        return [user, userInfo];
       },
-    });
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      }
+    );
+
     return res.status(201).json({ message: "회원가입이 완료되었습니다." });
   } catch (err) {
     next(err);
