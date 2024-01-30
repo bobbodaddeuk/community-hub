d1049ce6fa0c2ae63e70f1f60b998d6ea892dc50
TriumphOne <woo001232naver.com>
2024-01-30 22:10:11 +0900

    dotenv 적용

--- src/app.js
+++ src/app.js
@@ -5,14 +5,40 @@ import PostsRouter from "./routes/posts.router.js";
 import CommentsRouter from "./routes/comments.router.js";
 import LogMiddleware from "./middlewares/log.middleware.js";
 import ErrorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
+import expressSession from "express-session";
+import expressMySQLSession from "express-mysql-session";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3018;
 
+const MySQLStore = expressMySQLSession(expressSession);
+const sessionStore = new MySQLStore({
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  expiration: 1000 * 60 * 60 * 24,
+  createDatabaseTable: true,
+});
+
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, //1초 x 60 x 60 x 24 = 24시간 동안 쿠키를 사용할 수 있도록 설정
+    },
+    store: sessionStore,
+  })
+);
 app.use("/api", [UsersRouter, PostsRouter, CommentsRouter]);
 app.use(ErrorHandlingMiddleware);
 
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -63,9 +63,8 @@ router.post("/sign-in", async (req, res, next) => {
   if (!(await bcrypt.compare(password, user.password)))
     return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
 
-  const token = jwt.sign({ userId: user.userId }, "custom-secret-key");
+  req.session.userId = user.userId;
 
-  res.cookie("authorization", `Bearer ${token}`);
   return res.status(200).json({ message: "로그인에 성공하였습니다." });
 });
 
@@ -94,6 +93,52 @@ router.get("/users", authMiddleware, async (req, res, next) => {
     },
   });
   return res.status(200).json({ data: user });
+});
+
+router.patch("/users", authMiddleware, async (req, res, next) => {
+  const updatedData = req.body;
+  const { userId } = req.user;
+
+  const userInfo = await prisma.usersInfos.findFirst({
+    where: { userId: +userId },
+  });
+  if (!userInfo)
+    return res
+      .status(404)
+      .json({ message: "사용자 정보가 존재하지 않습니다." });
+
+  await prisma.$transaction(
+    async (tx) => {
+      await tx.usersInfos.update({
+        data: {
+          ...updatedData,
+        },
+        where: {
+          userId: +userId,
+        },
+      });
+
+      for (let key in updatedData) {
+        if (userInfo[key] !== updatedData[key]) {
+          await tx.userHistories.create({
+            data: {
+              userId: +userId,
+              changedField: key,
+              oldValue: String(userInfo[key]),
+              newValue: String(updatedData[key]),
+            },
+          });
+        }
+      }
+    },
+    {
+      isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+    }
+  );
+
+  return res
+    .status(200)
+    .json({ message: "사용자 정보 변경에 성공하였습니다." });
 });
 
 export default router;
